cccec5f6590b7a53a07e173db4daf69122c4989c
AnnaKotovskaya <anna.glushakova07@gmail.com>
2023-06-15 21:47:24 +0300

    Test with JaCoCo

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMaxIncomes() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 17, 8, 6, 11, 11, 12};
+        long expected = 17;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
